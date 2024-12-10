b8238c68d76d7195e23f21d883d11f40825dc479
chuong-lee <chuongldps37998@gmail.com>
2024-12-10 20:16:29 +0700

    them san pham

--- public/models/orderDetail.model.php
+++ public/models/orderDetail.model.php
@@ -1,28 +1,27 @@
-<?php
+    <?php
 
-class OrderDetailModel{
-    private $db;
-    public function __construct(){
-        $this->db = new Database();
+    class OrderDetailModel
+    {
+        private $db;
+        public function __construct()
+        {
+            $this->db = new Database();
+        }
+        public function insertOrderDetail($data)
+        {
+            $sql = "INSERT INTO `orderdetail` (id_product, id_order, quantity) VALUES (?, ?, ?)";
+            $params = [$data['id_product'], $data['id_order'], $data['quantity']];
+            return $this->db->insert($sql, $params);
+        }
+        public function getOrderDetail($userId, $status)
+        {
+            $sql = 'SELECT p.name, p.price, p.image, o.quantity, o2.status, o2.id, o2.id_user 
+                FROM product p 
+                JOIN orderdetail o ON o.id_product = p.id 
+                JOIN `order` o2 ON o2.id = o.id_order 
+                JOIN category c ON c.id = p.id_cate 
+                WHERE o2.id_user = ? AND o2.status = ?';
+            return $this->db->getAllInfoOrder($sql, [$userId, $status]);
+        }
     }
-
-    public function insertOrderDetail($data){
-        $sql = "INSERT INTO `orderdetail` (id_product, id_order, quantity) VALUES (?, ?, ?)";
-        $params = [$data['id_product'], $data['id_order'], $data['quantity']];
-        return $this->db->insert($sql, $params);
-    }
-
-    public function getOrderDetail($userId, $status) {
-        $sql = 'SELECT p.name, p.price, p.image, o.quantity, o2.status, o2.id, o2.id_user 
-            FROM product p 
-            JOIN orderdetail o ON o.id_product = p.id 
-            JOIN `order` o2 ON o2.id = o.id_order 
-            JOIN category c ON c.id = p.id_cate 
-            WHERE o2.id_user = ? AND o2.status = ?';
-
-        return $this->db->getAllInfoOrder($sql, [$userId, $status]);
-    }
-}
-
-
-?>
+    ?>
