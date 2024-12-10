dcf46c4539a54d1cd5b821617f32b2eefe9e362c
chuong-lee <chuongldps37998@gmail.com>
2024-12-11 00:18:03 +0700

    sửa

--- app/views/content/banner.header.php
+++ app/views/content/banner.header.php
@@ -5,9 +5,9 @@
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
     <div class="carousel-inner">
-      <div class="carousel-item active"><img class="d-block w-100 carousel-img" src="../public/client/images/banner-1.jpg"></div>
-      <div class="carousel-item"><img class="d-block w-100" src="../public/client/images/banner-1.jpg"></div>
-      <div class="carousel-item"><img class="d-block w-100" src="../public/client/images/banner-1.jpg"></div>
+      <div class="carousel-item active"><img class="d-block w-100 carousel-img" src="../public/client/images/blog-1.png"></div>
+      <div class="carousel-item"><img class="d-block w-100" src="../public/client/images/blog-2.png"></div>
+      <div class="carousel-item"><img class="d-block w-100" src="../public/client/images/blog-3.png"></div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span></button>
     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden">Next</span></button>
--- app/views/content/sanPham.php
+++ app/views/content/sanPham.php
@@ -1,6 +1,8 @@
 <section>
     <div class="product">
         <div class="container">
+            <?php require_once 'banner.header.php';?>
+
             <!-- Category Tabs -->
             <div class="tab-pane fade w-100" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="1">
                 <!-- <?php
@@ -28,38 +30,6 @@
             </div>
 
             <!-- Carousel Section -->
-            <div id="carouselExampleIndicator" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                    <button class="active" type="button" data-bs-target="#carouselExampleIndicators"
-                            data-bs-slide-to="0" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                            aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                            aria-label="Slide 3"></button>
-                </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-100 carousel-img" src="../public/client/images/banner-1.jpg"
-                             alt="Banner 1">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="../public/client/images/banner-1.jpg" alt="Banner 2">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-100" src="../public/client/images/banner-1.jpg" alt="Banner 3">
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-                        data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-                        data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
 
             <!-- Product Grid -->
             <div class="container">
