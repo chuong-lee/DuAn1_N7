01b165da209b71a6fff18e626bd9675c660de9a0
chuong-lee <chuongldps37998@gmail.com>
2024-12-10 23:57:47 +0700

    sửa

--- app/views/content/tintuc.php
+++ app/views/content/tintuc.php
@@ -10,51 +10,31 @@
                 </ol>
                 <div class="row gy-5">
                     <?php
-                        $trsp = '';
-                        foreach ($sp->mangsp as $key => $value) {
-                            // Kiểm tra dữ liệu trước khi sử dụng
-                            $id = htmlspecialchars($value['id'] ?? '');
-                            $title = htmlspecialchars($value['title'] ?? '');
-                            $created_at = htmlspecialchars($value['created_at'] ?? '');
-
-                            $trsp .= '
-                                <div class="col-lg-4 col-md-6">
-                                    <div class="border border-light border-1 rounded-3 hvr-float">
-                                        <div class="w-100 p-2">
-                                            <!-- Hình ảnh bài viết -->
-                                            <div class="post-img w-100">
-                                                <img class="img-fluid w-100 rounded-3" 
-                                                     src="../public/client/images/blog/blog-' . $id . '.jpg" 
-                                                     alt="' . $title . '">
-                                            </div>
-                            
-                                            <!-- Thông tin bài viết -->
+                    $trsp = '';
+                    foreach ($sp->mangsp as $key => $value) {
+                        $trsp .= '
+                                    <div class="col-lg-4 col-md-6">
+                                        <div class="border border-light border-1 rounded-3 hvr-float">
+                                            <div class="w-100 p-2">
+                                            <a href="index.php?page=ctbaiviet&id='.$value['id'].'"><div class="post-img w-100"><img class="img-fluid w-100 rounded-3" src="../public/client/images/blog/img'.$value['id'].'.jpg" alt=""></div></a>
                                             <div class="meta-top">
                                                 <ul class="p-0 text-start">
-                                                    <li class="d-flex align-items-center">
-                                                        <a class="text-black" href="index.php?page=ctbaiviet&id=' . $id . '">Xem</a>
-                                                    </li>
-                                                    <li class="d-flex align-items-center">
-                                                        <i class="bi bi-dot"></i>
-                                                        <a class="text-black" href="">
-                                                            <time datetime="' . $created_at . '">' . $created_at . '</time>
-                                                        </a>
-                                                    </li>
+                                                <li class="d-flex align-items-center"><a class="text-black" href="index.php?page=ctbaiviet&id='.$value['id'].'">Mới</a></li>
+                                                <li class="d-flex align-items-center"><i class="bi bi-dot"></i><a class="text-black" href="">
+                                                    <time datetime="2022-01-01">'.$value['created_at'].'</time></a></li>
                                                 </ul>
                                             </div>
-                            
-                                            <!-- Tiêu đề bài viết -->
-                                            <p class="title text-start fs-4 text-clamp">' . $title . '</p>
-                            
-                                            <!-- Nút hành động -->
-                                            <div class="btn btn-primari p-3 hvr-float">READ MORE</div>
-                                        </div>
+                                        <a href="index.php?page=ctbaiviet&id='.$value['id'].'"><p class="title text-start text-black fs-4">'.$value['title'].'</p></a>
+                                        <a href="index.php?page=ctbaiviet&id='.$value['id'].'">
+                                    <div class="btn btn-primari p-3 hvr-float">Xem Thêm</div></a>
                                     </div>
                                 </div>
+                                </div>
                             ';
-                        }
-                        echo $trsp;
+                    }
+                    echo $trsp;
                     ?>
+
                 </div>
     </section>
     <!-- main js-->
